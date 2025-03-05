b78496941050b7021aa9b9d58ca5b872d0483a7d
Jeyshel <149998953+Jeyshel@users.noreply.github.com>
2025-03-05 16:41:40 +0800

    mongodb connection

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ require('dotenv').config()
 
 const express = require('express') // Fixed typo in require
 const expressLayouts = require('express-ejs-layouts')
-
+const connectDB = require('./server/config/db')
+connectDB()
 const app = express()
-const PORT = process.env.PORT || 5000
-
-app.use(express.static('public'))
-
+const PORT = 5000 || process.env.PORT
+ 
+app.use(express.static('public')) 
+  
 //templating Engine
 app.use(expressLayouts)
 app.set('layout', './layouts/main')
 app.set('view engine','ejs')
+ 
+
 
 app.use('/', require('./server/routes/main'))
 
