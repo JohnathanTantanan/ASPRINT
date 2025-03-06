0d4f8d704c295f16656c46ac598d2e3cb6b5ef17
JohnathanTantanan <lance_xavier_lim@dlsu.edu.ph>
2025-03-06 16:46:52 +0800

    register wip

---
+++ server/models/user.js
@@ -0,0 +1,27 @@
+const { type } = require('express/lib/response')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const UserSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    picture:{
+        type: String,
+        required: false
+    },
+    description:{
+        type: String,
+        required: false
+    },
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    communities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Community' }],
+})
+
+module.exports = mongoose.model('User', UserSchema)
