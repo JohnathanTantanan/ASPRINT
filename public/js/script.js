b7c88376042542d3b48f7e4dada5959b2987e91a
JohnathanTantanan <lance_xavier_lim@dlsu.edu.ph>
2025-03-07 11:02:50 +0800

    working login and register
    -i hashed the passwords
    
    WIP DASHBOARD / USER SESSIONS

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    document.getElementById('upvote-btn').onclick = function(){
-        
-    };
-    
-    });
--- server/routes/login.js
+++ server/routes/login.js
@@ -4,6 +4,27 @@ const dataModule = require('../../data')
 //Server Model/Schema
 const Post = require('../models/Post')
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+/**GET /
+ * LOGIN AND REGISTER
+ */
+const authMiddleware = async (req,res,next) => {
+        const token = req.cookies.token;
+
+        if(!token){
+            return res.status(401).json({message: 'Unauthorized'});
+        }
+
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.userId = decoded.userId;
+            next();
+        } catch (error) {
+            return res.status(401).json({message: 'Unauthorized'});
+        }
+}
 
 /**GET /
  * LOGIN AND REGISTER
@@ -26,6 +47,15 @@ router.get('/register', async(req,res)=>{
     }
 });
 
+router.get('/dashboard', authMiddleware, async(req,res)=>{
+    try {
+        res.render('dashboard', {
+            layout: 'layouts/main'})
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 /**POST /
  * LOGIN AND REGISTER
  */
@@ -34,8 +64,24 @@ router.post('/login', async (req,res)=>{
 
 
         const { username, password } = req.body;
-        console.log(req.body);
-        res.redirect('/login');
+        
+        const user = await User.findOne({username});
+
+        if(!user){
+            return res.status(401).json({message: 'Invalid Credentials'});
+        } 
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(401).json({message: 'Invalid Credentials'});
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        res.cookie('token', token, {
+            httpOnly: true
+        });
+        res.redirect('/dashboard');
 
 
     } catch (error) {
@@ -46,15 +92,15 @@ router.post('/login', async (req,res)=>{
 router.post('/register', async (req,res)=>{
     try {
         const { username, password, confirm } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         if (password !== confirm) {
-            console.log('Passwords do not match');
-            res.redirect('/register');
+            res.status(408).json({message: 'Passwords do not match'});
         } else {
             try {
                 const user = await User.create({
                     username,
-                    password
+                    password: hashedPassword
                 });
                 res.status(201).json({message: 'User Created', user});
                 res.redirect('/login');
