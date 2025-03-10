35079dc9bc4ee41a9d46793a830bc9d7f4ac22ce
Exabyte50 <gerardo_velasquez@dlsu.edu.ph>
2025-03-10 16:13:55 +0800

    Upvote progress
    upvoting works on previews, not in post page. downvote still does not work.

--- public/js/script.js
+++ public/js/script.js
@@ -16,25 +16,33 @@ function toggleEditMode(editFormId, displayElementsIds, editButtonId) {
 
 // JQUERY/AJAX FUNCTIONS
 $(document).ready(function(){
+    // make function modular for both votes
     function upvotePost(postId){
-        // const postId = $(this).attr('postId');
         console.log('Upvoting post:', postId);
         $.ajax({
             url: `/post/upvote/${postId}`,
             method: 'POST',
             success: function(result) {
                 if (result.success) {
-                    $(this).closest('button').find('.upvote-count').text(result.upvotes);
+                    const upvotes = $(this).find('.upvote-count');
+                    if (upvotes.length) {
+                        upvotes.text(result.upvotes);
+                        console.log('Upvote count updated:', result.upvotes);
+                    } else {
+                        console.error('Upvote count element not found');
+                    }
                 }
-            }.bind(this),
+            },
             error: function(xhr, status, error) {
                 console.error('Failed to upvote post:', error);
             }
         });
     }
 
+    // Event Listeners
     $('.upvote-btn').click(function(){
-        upvotePost($(this).attr('postId'));
-        //console.log('Upvoting post...');
+        const postId = $(this).attr('postId');
+        console.log('Button clicked, postId:', postId);
+        upvotePost(postId);
     });
 });
