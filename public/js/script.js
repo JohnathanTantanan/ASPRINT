34313e42ad9e6c7261c85b93658527a09b7ae3c5
Exabyte50 <gerardo_velasquez@dlsu.edu.ph>
2025-03-10 11:26:48 +0800

    fiddling with ajax

--- public/js/script.js
+++ public/js/script.js
@@ -18,21 +18,23 @@ function toggleEditMode(editFormId, displayElementsIds, editButtonId) {
 $(document).ready(function(){
     function upvotePost(postId){
         // const postId = $(this).attr('postId');
-        $.ajax({
-            url: `/post/upvote/${postId}`,
-            method: 'POST',
-            success: function(result) {
-                if (result.success) {
-                    $(this).closest('button').find('.upvote-count').text(result.upvotes);
-                }
-            }.bind(this),
-            error: function(xhr, status, error) {
-                console.error('Failed to upvote post:', error);
-            }
-        });
+        console.log('Upvoting post:', postId);
+        // $.ajax({
+        //     url: `/post/upvote/${postId}`,
+        //     method: 'POST',
+        //     success: function(result) {
+        //         if (result.success) {
+        //             $(this).closest('button').find('.upvote-count').text(result.upvotes);
+        //         }
+        //     }.bind(this),
+        //     error: function(xhr, status, error) {
+        //         console.error('Failed to upvote post:', error);
+        //     }
+        // });
     }
 
     $('.upvote-button').click(function(){
-        upvotePost($(this).attr('postId'));
+        //upvotePost($(this).attr('postId'));
+        console.log('Upvoting post...');
     });
 });
